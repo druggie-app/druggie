26ca61d8bf6e0693b40284fc9af9375d6e4ed921
katja <kk@thesocialcollective.co>
2023-05-10 12:31:27 +0200

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/druggie/" : "/",
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/"
 }
